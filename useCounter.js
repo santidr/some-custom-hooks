b0dae87693c048c4ebde4b5395d6ebf7e96b1589
Santiago Diaz <kerbindr@gmail.com>
2021-03-06 00:31:25 -0400

    Init commit

---
+++ useCounter.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+const useCounter = (initialState = 0) => {
+
+    const [counter, setCounter] = useState(initialState)
+
+    const increment = () => setCounter(counter + 1)
+
+    const decrement = () => {
+        if (counter > 0)
+            setCounter(counter - 1)
+    }
+
+    const reset = () => setCounter(initialState)
+
+    return ({
+        counter,
+        increment,
+        decrement,
+        reset
+    });
+}
+ 
+export default useCounter;
